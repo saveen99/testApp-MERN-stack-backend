3a889232e82d4738742beb89bdba8d8c5fb9c8fd
Saveen Maduranga <saveenonline@gmail.com>
2024-04-05 22:54:26 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+
+module.exports = app;
