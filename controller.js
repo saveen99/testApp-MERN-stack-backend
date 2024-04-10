125fb8f0b921310649068e9932fba2de1ba34e05
saveen-maduranga <saveenonline@gmail.com>
2024-04-10 19:28:51 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -7,11 +7,45 @@ const getUsers = (req, res, next) => {
             res.json({ response })
         })
         .catch(error => {
-            res.json({ message: error })
-        })
+            res.json({ error })
+        });
 };
 
+const addUser = (req, res, next) => {
+    const user = new User({
+        id: req.body.id,
+        name: req.body.name,
+    });
+    user.save()
+        .then(response => {
+            res.json({ response })
+        })
+        .catch(error => {
+            res.json({ error })
+        });
+};
 
+const updateUser = (req, res, next) => {
+    const { id,name } = req.body;
+    User.updateOne({ id: id}, { $set: { name: name } })
+        .then(response => {
+            res.json({ response })
+        })
+        .catch(error => {
+            res.json({ error })
+        });
+};
+
+const deleteUser = (req, res, next) => {
+    const id = req.body.id;
+    User.deleteOne({ id: id })
+        .then(response => {
+            res.json({ response })
+        })
+        .catch(error => {
+            res.json({ error })
+        });   
+};
 
 exports.getUsers = getUsers;
 exports.getUsersById = getUsersById;
