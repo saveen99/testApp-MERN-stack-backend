a4cb368e3b99862e9979d245a70c81f64826a6ab
saveen-maduranga <saveenonline@gmail.com>
2024-04-12 18:35:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = 3001;
 const host = '127.0.0.1';
 const mongoose = require('mongoose');
+const router = require('./router');
 
 app.use(cors());
 app.use(express.json());
@@ -25,3 +26,4 @@ const server = app.listen(port, host, () => {
     console.log(`Node server is Listening to ${server.address().port}`)
 });
 
+app.use('/api', router);
